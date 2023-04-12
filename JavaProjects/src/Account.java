6b4f2ee7cda43c027482593d26b38ec9ce35c56f
Mantra Mehta <mmehta2@toromail.csudh.edu>
2023-04-12 14:55:49 -0700

    pushing Account

---
+++ JavaProjects/src/Account.java
@@ -0,0 +1,106 @@
+// Mantra Mehta(mmehta2@toromail.csudh.edu)
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Account implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String accountName;
+	private Customer accountHolder;
+	private ArrayList<Transaction> transactions;
+	
+	private boolean open=true;
+	private int accountNumber;
+	String Currency;
+
+	public Account(String name, Customer customer, String Cur) {
+		accountName=name;
+		accountHolder=customer;
+		transactions=new ArrayList<Transaction>();
+		accountNumber=UniqueCounter.nextValue();
+		Currency = Cur;
+	}
+	
+	public String getAccountName() {
+		return accountName;
+	}
+
+	public Customer getAccountHolder() {
+		return accountHolder;
+	}
+
+	public double getBalance() {
+		double workingBalance=0;
+		
+		for(Transaction t: transactions) {
+			if(t.getType()==Transaction.CREDIT)workingBalance+=t.getAmount();
+			else workingBalance-=t.getAmount();
+		}
+		
+		return workingBalance;
+	}
+	
+	
+	
+	
+	public void deposit(double amount)  throws AccountClosedException{
+		double balance=getBalance();
+		if(!isOpen()&&balance>=0) {
+			throw new AccountClosedException("\nAccount is closed with positive balance, deposit not allowed!\n\n");
+		}
+		transactions.add(new Transaction(Transaction.CREDIT,amount));
+	}
+	
+	
+	
+	
+	public void withdraw(double amount) throws InsufficientBalanceException {
+			
+		double balance=getBalance();
+			
+		if(!isOpen()&&balance<=0) {
+			throw new InsufficientBalanceException("\nThe account is closed and balance is: "+balance+"\n\n");
+		}
+		
+		transactions.add(new Transaction(Transaction.DEBIT,amount));
+	}
+	
+	public void close() {
+		open=false;
+	}
+	
+	public boolean isOpen() {
+		return open;
+	}
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public String toString() {
+		String aName=accountNumber+"("+accountName+")"+" : "+accountHolder.toString()+ " : "+ Currency +" : "+getBalance()+" : "+ getBalance()*Bank.Rate(Currency)+ " : "+(open?"Account Open":"Account Closed");
+		return aName;
+	}
+	 
+	public void printTransactions(OutputStream out) throws IOException {
+		
+		out.write("\n\n".getBytes());
+		out.write("------------------\n".getBytes());
+	
+		for(Transaction t: transactions) {
+			out.write(t.toString().getBytes());
+			out.write((byte)10);
+		}
+		out.write("------------------\n".getBytes());
+		out.write(("Balance: "+getBalance()+"\n\n\n").getBytes());
+		out.flush();
+		
+	}
+	
+	public void printDetail(OutputStream out) throws IOException {
+		out.write(("\nAccount Number: "+accountNumber+"\n"+"Name: "+accountHolder.getFirstName()+" "+accountHolder.getLastName() +"\n"+"SSN: "+accountHolder.getSSN()+"\n"+"Currency: "+Currency+"\n"+"Currency Balance: "+getBalance()+"\n"+"USD Balance: "+ getBalance()*Bank.Rate(Currency)+"\n\n").getBytes());
+		out.flush();
+	}
+}
