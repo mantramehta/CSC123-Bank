9f4725b36e93612ce3ad9b4bf71048cf4886b9a2
Mantra Mehta <mmehta2@toromail.csudh.edu>
2023-04-12 15:00:24 -0700

    pushing exchange rate

---
+++ CSC123-Bank-Project/Bank/com/usman/csudh/bank/core/ExchangeRate.java
@@ -0,0 +1,18 @@
+package com.usman.csudh.bank.core;
+public class ExchangeRate {
+    private String name;
+    private double rate;
+
+    public ExchangeRate(String name, double rate) {
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+}
