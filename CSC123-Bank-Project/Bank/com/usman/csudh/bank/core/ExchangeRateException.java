1bb10dd63c46a698ca7f85963fdadb5f56c3ba8f
Mantra Mehta <mmehta2@toromail.csudh.edu>
2023-04-12 17:26:07 -0700

    committing Exceptionclasses

---
+++ CSC123-Bank-Project/Bank/com/usman/csudh/bank/core/ExchangeRateException.java
@@ -0,0 +1,8 @@
+// Mantra Mehta(mmehta2@toromail.csudh.edu)
+public class ExchangeRateException extends Exception {
+
+	public ExchangeRateException(String message) {
+		super(message);
+	}
+
+}
---
+++ CSC123-Bank-Project/Bank/com/usman/csudh/bank/core/USDNotFoundException.java
@@ -0,0 +1,9 @@
+// Mantra Mehta(mmehta2@toromail.csudh.edu)
+public class USDNotFoundException extends Exception {
+
+	public USDNotFoundException(String message) {
+		super(message);
+	}
+
+}
+
